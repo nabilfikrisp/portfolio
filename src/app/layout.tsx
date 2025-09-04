54a459365f3b3b5e679613c1ba65c431b655fd27
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-09-04 12:35:21 +0700

    chore(font): change font to outfit, jakarta sans, and geist mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Outfit, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const outfit = Outfit({
+  variable: "--font-outfit",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const jakartaSans = Plus_Jakarta_Sans({
+  variable: "--font-jakarta-sans",
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.variable} ${jakartaSans.variable} ${geistMono.variable}`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,21 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
+      <h1 className="text-4xl">Muhammad Nabil Fikri Sudjarpadi Putra</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus deleniti
+        ut sit quia explicabo doloribus nam amet, error, delectus dolorum
+        placeat molestias quam ratione cupiditate quos debitis reprehenderit
+        distinctio enim ducimus quisquam sed temporibus perferendis. Natus
+        deleniti aspernatur impedit recusandae voluptas minus facilis,
+        consectetur accusamus quam sequi laboriosam ullam voluptates. Rem
+        dolorem consequatur in ipsum? Nostrum officiis non exercitationem iusto
+        facilis eveniet possimus magnam atque? Consequatur maiores aliquid
+        laborum impedit laudantium rerum, omnis cupiditate accusamus modi
+        placeat sint exercitationem? Repellendus velit dolores consequuntur
+        ipsum inventore veritatis saepe, quas rem facere, atque a! Consectetur
+        quaerat natus aut facilis quibusdam. Officia, eaque!
+      </p>
       <Button>Click me</Button>
     </div>
   );
