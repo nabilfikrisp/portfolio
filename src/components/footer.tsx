723390c46c17e70fa400129cf679d685047959a0
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-09-04 13:43:44 +0700

    feat: added footer and navbar

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,22 @@
 export function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="border-t-my-accent-green border-t py-3">
-      <div className="my-max-width">Footer</div>
+      <div className="my-max-width flex flex-col items-center justify-between gap-4 md:flex-row">
+        <div className="flex flex-col">
+          <h3 className="text-my-accent-one text-center text-lg font-semibold lg:text-start">
+            nabilfikrisp
+          </h3>
+          <p className="text-muted-foreground text-sm dark:text-gray-400">
+            Full Stack Developer
+          </p>
+        </div>
+      </div>
+
+      <div className="text-muted-foreground border-muted-foreground my-max-width mt-6 border-t pt-6 text-center text-sm">
+        <p>© {currentYear} nabilfikrisp. All rights reserved.</p>
+        <p className="mt-2">Developed & Built with ❤️</p>
+      </div>
     </footer>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,11 @@
 export function Navbar() {
   return (
     <header className="border-b-my-accent-green sticky top-0 z-50 border-b py-3 backdrop-blur-sm">
-      <div className="my-max-width">Navbar</div>
+      <nav className="my-max-width flex items-center gap-4">
+        <span className="font-outfit mr-auto text-3xl font-semibold tracking-tighter hover:cursor-pointer">
+          nabilfikrisp
+        </span>
+      </nav>
     </header>
   );
 }
