2f24a812e3a48a3f6b4930b4b67efa966700a0c5
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-09-06 15:40:16 +0700

    feat: add animated tabs from shadcn.io

--- src/components/sections/experience.section.tsx
+++ src/components/sections/experience.section.tsx
@@ -1,4 +1,10 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tabs,
+  TabsContent,
+  TabsContents,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/shadcn-io/tabs";
 import { EducationHistoryList } from "../education-history-list";
 import { WorkExperienceList } from "../work-experience-list";
 
@@ -10,26 +16,32 @@ export function ExperienceSection() {
     >
       <h1 className="text-my-headline text-3xl font-semibold">Experience</h1>
       <Tabs defaultValue="work">
-        <TabsList className="bg-my-accent-green w-full">
+        <TabsList
+          className="bg-my-accent-green w-full"
+          activeClassName="bg-my-accent-orange"
+        >
           <TabsTrigger
             value="work"
-            className="text-my-white data-[state=active]:bg-my-accent-orange"
+            className="text-my-white data-[state=active]:text-my-white data-[state=active]:bg-my-accent-orange"
           >
             Work
           </TabsTrigger>
           <TabsTrigger
             value="education"
-            className="text-my-white data-[state=active]:bg-my-accent-orange"
+            className="text-my-white data-[state=active]:text-my-white data-[state=active]:bg-my-accent-orange"
           >
             Education
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="work">
-          <WorkExperienceList />
-        </TabsContent>
-        <TabsContent value="education">
-          <EducationHistoryList />
-        </TabsContent>
+
+        <TabsContents>
+          <TabsContent value="work">
+            <WorkExperienceList />
+          </TabsContent>
+          <TabsContent value="education">
+            <EducationHistoryList />
+          </TabsContent>
+        </TabsContents>
       </Tabs>
     </section>
   );
