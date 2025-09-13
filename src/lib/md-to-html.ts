c63a2a458537e4e69e56aa8f827b5d8df136c7ad
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-09-13 14:12:54 +0700

    feat: add fine grained bundle of shiki js

--- src/lib/md-to-html.ts
+++ src/lib/md-to-html.ts
@@ -1,20 +1,37 @@
-import rehypeShiki from "@shikijs/rehype";
+import rehypeShikiFromHighlighter from "@shikijs/rehype/core";
 import rehypeStringify from "rehype-stringify";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
+import { createHighlighter } from "shiki/bundle/web";
+import { createOnigurumaEngine } from "shiki/engine/oniguruma";
 import { unified } from "unified";
+
+const highlighter = await createHighlighter({
+  themes: [
+    import("@shikijs/themes/github-light"),
+    import("@shikijs/themes/tokyo-night"),
+  ],
+  langs: [
+    import("@shikijs/langs/javascript"),
+    import("@shikijs/langs/typescript"),
+    import("@shikijs/langs/tsx"),
+    import("@shikijs/langs/markdown"),
+    import("@shikijs/langs/json"),
+    import("@shikijs/langs/css"),
+    import("@shikijs/langs/html"),
+    import("@shikijs/langs/bash"),
+    import("@shikijs/langs/yaml"),
+  ],
+  engine: createOnigurumaEngine(import("shiki/wasm")),
+});
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypeShiki, {
-      inline: "tailing-curly-colon",
-      themes: {
-        light: "github-light",
-        dark: "tokyo-night",
-      },
-    })
+    .use(() =>
+      rehypeShikiFromHighlighter(highlighter, { theme: "github-light" }),
+    )
     .use(rehypeStringify)
     .process(markdown);
   return result.toString();
