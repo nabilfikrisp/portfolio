5350a09cfcae9511c5bc12fe2a656fa11e48eb17
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-09-15 18:59:49 +0700

    feat: update blog page layout and add PostList component for simpler post display

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,8 +1,7 @@
 import { HOSTING_URL, PROFILE_PICTURE_URL } from "@/lib/consts";
 import { getAllPosts } from "@/lib/post-api";
 import { Metadata } from "next";
-import { HeroPost } from "./_components/hero-post";
-import { MoreStories } from "./_components/more-stories";
+import { PostList } from "./_components/post-list";
 
 export const metadata: Metadata = {
   title: "nabilfikrisp | blogs",
@@ -23,20 +22,36 @@ export const metadata: Metadata = {
 
 export default function BlogPage() {
   const allPosts = getAllPosts();
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
 
   return (
-    <div className="max-width-app flex w-full flex-1 flex-col items-center justify-center py-8">
-      <HeroPost
-        title={heroPost.title}
-        coverImage={heroPost.coverImage}
-        date={heroPost.date}
-        author={heroPost.author}
-        slug={heroPost.slug}
-        excerpt={heroPost.excerpt}
-      />
-      {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+    <div className="max-width-app mx-auto flex w-full flex-1 flex-col items-center justify-center py-8">
+      <header className="mb-6 flex w-full flex-col items-center gap-4 md:flex-row">
+        <div className="border-my-accent-green flex h-fit w-fit items-center justify-center place-self-center overflow-hidden rounded-full border-2 p-1 sm:place-self-auto">
+          <img
+            className="h-40 w-40 rounded-full object-cover"
+            src={PROFILE_PICTURE_URL}
+            alt="profile-picture"
+            title="Profile Picture"
+            width={160}
+            height={160}
+          />
+        </div>
+        <div className="flex-1 text-center md:text-left">
+          <h1 className="text-my-headline mb-2 text-3xl font-semibold tracking-tight md:text-4xl">
+            nabilfikrisp
+          </h1>
+          <p className="font-heading text-lg md:text-xl">
+            Learning journey, coding experiments, and lessons learned
+          </p>
+        </div>
+      </header>
+
+      <div className="flex w-full flex-col gap-4">
+        <h2 className="text-center text-2xl font-bold md:text-left md:text-4xl">
+          Blog Posts
+        </h2>
+        <PostList posts={allPosts} />
+      </div>
     </div>
   );
 }
